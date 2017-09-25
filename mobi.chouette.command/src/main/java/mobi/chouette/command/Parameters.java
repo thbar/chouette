0214324b71fcd920349798ee6b5aa5bd5cfa9a51
mhicauber <mhicauber@okina.fr>
2017-09-25 14:55:06 +0200

    v3.4.3 okina

--- mobi.chouette.command/src/main/java/mobi/chouette/command/Parameters.java
+++ mobi.chouette.command/src/main/java/mobi/chouette/command/Parameters.java
@@ -1,15 +1,10 @@
 package mobi.chouette.command;
 
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlElements;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlType;
-
 import lombok.Data;
 import mobi.chouette.exchange.parameters.AbstractParameter;
 import mobi.chouette.exchange.validation.parameters.ValidationParameters;
+
+import javax.xml.bind.annotation.*;
 
 @XmlRootElement
 @XmlAccessorType(XmlAccessType.FIELD)
--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/report/ActionReport.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/report/ActionReport.java
@@ -1,267 +1,258 @@
 package mobi.chouette.exchange.report;
 
-import java.io.PrintStream;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import javax.xml.bind.annotation.XmlAccessType;
-import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
-import javax.xml.bind.annotation.XmlRootElement;
-import javax.xml.bind.annotation.XmlTransient;
-import javax.xml.bind.annotation.XmlType;
-
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import mobi.chouette.common.Constant;
 import mobi.chouette.exchange.report.ActionReporter.FILE_STATE;
 import mobi.chouette.exchange.report.ActionReporter.OBJECT_TYPE;
-
 import org.codehaus.jettison.json.JSONArray;
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
 
+import javax.xml.bind.annotation.*;
+import java.io.PrintStream;
+import java.util.*;
+
 @XmlRootElement(name = "action_report")
 @XmlAccessorType(XmlAccessType.FIELD)
-@XmlType(propOrder = { "progression", "result", "zip", "files", "lines", "stats", "failure", "objects", "collections" })
+@XmlType(propOrder = {"progression", "result", "zip", "files", "lines", "stats", "failure", "objects", "collections"})
 @Data
 @EqualsAndHashCode(callSuper = false)
 public class ActionReport extends AbstractReport implements Constant, ProgressionReport, Report {
 
-	@XmlElement(name = "progression", required = true)
-	private Progression progression = new Progression();
+    @XmlElement(name = "progression", required = true)
+    private Progression progression = new Progression();
 
-	@XmlElement(name = "result", required = true)
-	private String result = ReportConstant.STATUS_OK;
+    @XmlElement(name = "result", required = true)
+    private String result = ReportConstant.STATUS_OK;
 
-	@XmlElement(name = "zip_files")
-	private List<FileReport> zips = new ArrayList<>();
+    @XmlElement(name = "zip_files")
+    private List<FileReport> zips = new ArrayList<>();
 
-	@XmlElement(name = "files")
-	private List<FileReport> files = new ArrayList<>();
+    @XmlElement(name = "files")
+    private List<FileReport> files = new ArrayList<>();
 
-	@XmlElement(name = "failure")
-	private ActionError failure;
+    @XmlElement(name = "failure")
+    private ActionError failure;
 
-	@XmlElement(name = "objects")
-	private Map<ActionReporter.OBJECT_TYPE, ObjectReport> objects = new HashMap<ActionReporter.OBJECT_TYPE, ObjectReport>();
+    @JsonIgnore
+    private Map<OBJECT_TYPE, ObjectReport> objects = new HashMap<>();
 
-	@XmlElement(name = "collections")
-	private Map<ActionReporter.OBJECT_TYPE, ObjectCollectionReport> collections = new HashMap<ActionReporter.OBJECT_TYPE, ObjectCollectionReport>();
+    @XmlElement(name = "objects")
+    private List<ObjectReport> objectsAsList = new ArrayList<>();
 
-	@XmlTransient
-	private Date date = new Date(0);
-	
-	/**
-	 * Find file report from name
-	 * 
-	 * @param name
-	 * @return
-	 */
-	protected FileReport findFileReport(String name) {
-		for (FileReport fileReport : files) {
-			if (fileReport.getName().equals(name))
-				return fileReport;
-		}
-		return null;
-	}
+    @JsonIgnore
+    private Map<OBJECT_TYPE, ObjectCollectionReport> collections = new HashMap<>();
 
-	/**
-	 * Find file report from name
-	 * 
-	 * @param name
-	 * @return
-	 */
-	protected FileReport findZipReport(String name) {
-		for (FileReport fileReport : zips) {
-			if (fileReport.getName().equals(name))
-				return fileReport;
-		}
-		return null;
-	}
+    @XmlElement(name = "collections")
+    private List<ObjectCollectionReport> collectionsAsList = new ArrayList<>();
 
-	/**
-	 * Find file report from name and state
-	 * 
-	 * @param name
-	 * @param state
-	 * @return
-	 */
-	protected FileReport findFileReport(String name, FILE_STATE state) {
-		for (FileReport fileReport : files) {
-			if (fileReport.getName().equals(name)
-					&& (fileReport.getStatus().name().equals(state.name())
-							|| FILE_STATE.OK.equals(fileReport.getStatus().name()) || FILE_STATE.OK
-								.equals(state.name()))) {
-				if (FILE_STATE.OK.equals(fileReport.getStatus().name()))
-					fileReport.setStatus(state);
-				return fileReport;
-			}
-		}
-		return null;
-	}
+    @XmlTransient
+    private Date date = new Date(0);
 
-	/**
-	 * set or unset error ; will set result to ERROR if error != null
-	 * 
-	 * @param error
-	 */
-	protected void setFailure(ActionError error) {
-		if (error == null) {
-			result = ReportConstant.STATUS_OK;
-			failure = null;
-		} else {
-			result = ReportConstant.STATUS_ERROR;
-			failure = error;
-		}
-	}
+    /**
+     * Find file report from name
+     *
+     * @param name
+     * @return
+     */
+    protected FileReport findFileReport(String name) {
+        for (FileReport fileReport : files) {
+            if (fileReport.getName().equals(name))
+                return fileReport;
+        }
+        return null;
+    }
 
-	/**
-	 * 
-	 * @param object
-	 */
-	protected void addObjectReport(ObjectReport object) {
-		if (!objects.containsKey(object.getType()))
-			objects.put(object.getType(), object);
-	}
+    /**
+     * Find file report from name
+     *
+     * @param name
+     * @return
+     */
+    protected FileReport findZipReport(String name) {
+        for (FileReport fileReport : zips) {
+            if (fileReport.getName().equals(name))
+                return fileReport;
+        }
+        return null;
+    }
 
-	/**
-	 * 
-	 * @param collection
-	 */
-	protected void addObjectCollectionReport(ObjectCollectionReport collection) {
-		if (!collections.containsKey(collection.getObjectType()))
-			collections.put(collection.getObjectType(), collection);
-	}
+    /**
+     * Find file report from name and state
+     *
+     * @param name
+     * @param state
+     * @return
+     */
+    protected FileReport findFileReport(String name, FILE_STATE state) {
+        for (FileReport fileReport : files) {
+            if (fileReport.getName().equals(name)
+                    && (fileReport.getStatus().name().equals(state.name())
+                    || FILE_STATE.OK.equals(fileReport.getStatus().name()) || FILE_STATE.OK
+                    .equals(state.name()))) {
+                if (FILE_STATE.OK.equals(fileReport.getStatus().name()))
+                    fileReport.setStatus(state);
+                return fileReport;
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * 
-	 * @param objectReport
-	 */
-	protected void addObjectReportToSpecificCollection(ObjectReport objectReport) {
-		ObjectCollectionReport collection = collections.get(objectReport.getType());
-		if (collection == null) {
-			collection = new ObjectCollectionReport();
-			collection.setObjectType(objectReport.getType());
-			addObjectCollectionReport(collection);
-		}
-		collection.addObjectReport(objectReport);
+    /**
+     * set or unset error ; will set result to ERROR if error != null
+     *
+     * @param error
+     */
+    protected void setFailure(ActionError error) {
+        if (error == null) {
+            result = ReportConstant.STATUS_OK;
+            failure = null;
+        } else {
+            result = ReportConstant.STATUS_ERROR;
+            failure = error;
+        }
+    }
 
-	}
+    /**
+     * @param object
+     */
+    protected void addObjectReport(ObjectReport object) {
+        if (!objects.containsKey(object.getType()))
+            objects.put(object.getType(), object);
+    }
 
-	/**
-	 * 
-	 * @param file
-	 */
-	protected void addFileReport(FileReport file) {
-		files.add(file);
-	}
+    /**
+     * @param collection
+     */
+    protected void addObjectCollectionReport(ObjectCollectionReport collection) {
+        if (!collections.containsKey(collection.getObjectType()))
+            collections.put(collection.getObjectType(), collection);
+    }
 
-	/**
-	 * 
-	 * @param file
-	 */
-	protected void addZipReport(FileReport file) {
-		zips.add(file);
-	}
+    /**
+     * @param objectReport
+     */
+    protected void addObjectReportToSpecificCollection(ObjectReport objectReport) {
+        ObjectCollectionReport collection = collections.get(objectReport.getType());
+        if (collection == null) {
+            collection = new ObjectCollectionReport();
+            collection.setObjectType(objectReport.getType());
+            addObjectCollectionReport(collection);
+        }
+        collection.addObjectReport(objectReport);
 
-	public JSONObject toJson() throws JSONException {
-		JSONObject actionReport = new JSONObject();
-		if (progression != null)
-			actionReport.put("progression", progression.toJson());
-		// "result","zip","files","lines","stats","failure"
-		actionReport.put("result", result);
-		if (!zips.isEmpty()) {
-			JSONArray array = new JSONArray();
-			actionReport.put("zip_files", array);
-			for (FileReport file : zips) {
-				array.put(file.toJson());
-			}
-		}
+    }
 
-		if (failure != null)
-			actionReport.put("failure", failure.toJson());
+    /**
+     * @param file
+     */
+    protected void addFileReport(FileReport file) {
+        files.add(file);
+    }
 
-		if (!files.isEmpty()) {
-			JSONArray array = new JSONArray();
-			actionReport.put("files", array);
-			for (FileReport file : files) {
-				array.put(file.toJson());
-			}
-		}
-		if (!objects.isEmpty()) {
-			JSONArray array = new JSONArray();
-			actionReport.put("objects", array);
-			for (ObjectReport object : objects.values()) {
-				array.put(object.toJson());
-			}
-		}
+    /**
+     * @param file
+     */
+    protected void addZipReport(FileReport file) {
+        zips.add(file);
+    }
 
-		if (!collections.isEmpty()) {
-			JSONArray array = new JSONArray();
-			actionReport.put("collections", array);
-			for (ObjectCollectionReport collection : collections.values()) {
-				array.put(collection.toJson());
-			}
-		}
+    public JSONObject toJson() throws JSONException {
+        JSONObject actionReport = new JSONObject();
+        if (progression != null)
+            actionReport.put("progression", progression.toJson());
+        // "result","zip","files","lines","stats","failure"
+        actionReport.put("result", result);
+        if (!zips.isEmpty()) {
+            JSONArray array = new JSONArray();
+            actionReport.put("zip_files", array);
+            for (FileReport file : zips) {
+                array.put(file.toJson());
+            }
+        }
 
-		JSONObject object = new JSONObject();
-		object.put("action_report", actionReport);
-		return object;
-	}
+        if (failure != null)
+            actionReport.put("failure", failure.toJson());
 
-	@Override
-	public boolean isEmpty() {
-		// used to know if report has to be saved
-		// Action Report has to be saved any time
-		return false;
-	}
+        if (!files.isEmpty()) {
+            JSONArray array = new JSONArray();
+            actionReport.put("files", array);
+            for (FileReport file : files) {
+                array.put(file.toJson());
+            }
+        }
+        if (!objects.isEmpty()) {
+            JSONArray array = new JSONArray();
+            actionReport.put("objects", array);
+            for (ObjectReport object : objects.values()) {
+                array.put(object.toJson());
+            }
+        }
 
-	public ObjectReport findObjectReport(String objectId, OBJECT_TYPE type) {
-		if (collections.containsKey(type)) {
-			ObjectCollectionReport collection = collections.get(type);
-			for (ObjectReport objectReport : collection.getObjectReports()) {
-				if (objectReport.getObjectId().equals(objectId))
-					return objectReport;
-			}
-		} else if (objects.containsKey(type)) {
-			return objects.get(type);
-		}
-		return null;
-	}
+        if (!collections.isEmpty()) {
+            JSONArray array = new JSONArray();
+            actionReport.put("collections", array);
+            for (ObjectCollectionReport collection : collections.values()) {
+                array.put(collection.toJson());
+            }
+        }
 
-	@Override
-	public void print(PrintStream out, StringBuilder ret , int level, boolean first) {
-		ret.setLength(0);
-		level = 0;
-		first = true;
-		out.print("{\"action_report\": {");
-		if (progression != null) {
-			printObject(out, ret, level + 1, "progression", progression, first);
-			first = false;
-		}
-		out.print(toJsonString(ret, level+1, "result", result, first));
+        JSONObject object = new JSONObject();
+        object.put("action_report", actionReport);
+        return object;
+    }
 
-		if (!zips.isEmpty())
-			printArray(out, ret, level + 1, "zip_files", zips, false);
-		if (failure != null)
-			printObject(out, ret, level + 1,"failure", failure,false);
-		if (!files.isEmpty())
-			printArray(out, ret, level + 1, "files", files, false);
-		if (!objects.isEmpty())
-			printArray(out, ret, level + 1, "objects", objects.values(), false);
-		if (!collections.isEmpty())
-			printArray(out, ret, level + 1, "collections", collections.values(), false);
-		
-		out.println("\n}}");
-	}
+    @Override
+    public boolean isEmpty() {
+        // used to know if report has to be saved
+        // Action Report has to be saved any time
+        return false;
+    }
 
-	@Override
-	public void print(PrintStream stream) {
-		print(stream, new StringBuilder() , 1, true);
+    public ObjectReport findObjectReport(String objectId, OBJECT_TYPE type) {
+        if (collections.containsKey(type)) {
+            ObjectCollectionReport collection = collections.get(type);
+            for (ObjectReport objectReport : collection.getObjectReports()) {
+                if (objectReport.getObjectId().equals(objectId))
+                    return objectReport;
+            }
+        } else if (objects.containsKey(type)) {
+            return objects.get(type);
+        }
+        return null;
+    }
 
-	}
+    @Override
+    public void print(PrintStream out, StringBuilder ret, int level, boolean first) {
+        ret.setLength(0);
+        level = 0;
+        first = true;
+        out.print("{\"action_report\": {");
+        if (progression != null) {
+            printObject(out, ret, level + 1, "progression", progression, first);
+            first = false;
+        }
+        out.print(toJsonString(ret, level + 1, "result", result, first));
+
+        if (!zips.isEmpty())
+            printArray(out, ret, level + 1, "zip_files", zips, false);
+        if (failure != null)
+            printObject(out, ret, level + 1, "failure", failure, false);
+        if (!files.isEmpty())
+            printArray(out, ret, level + 1, "files", files, false);
+        if (!objects.isEmpty())
+            printArray(out, ret, level + 1, "objects", objects.values(), false);
+        if (!collections.isEmpty())
+            printArray(out, ret, level + 1, "collections", collections.values(), false);
+
+        out.println("\n}}");
+    }
+
+    @Override
+    public void print(PrintStream stream) {
+        print(stream, new StringBuilder(), 1, true);
+
+    }
 }
