b2ff02a4aa6c5022a6dc844a22e4501b7dee7936
Mathieu Hicauber <mhicauber@okina.fr>
2018-07-03 14:13:00 +0200

    typo

--- mobi.chouette.service/src/main/java/mobi/chouette/service/JobService.java
+++ mobi.chouette.service/src/main/java/mobi/chouette/service/JobService.java
@@ -36,260 +36,262 @@ import java.util.logging.Logger;
 @ToString(exclude = {"inputValidator"})
 public class JobService implements JobData, ServiceConstants {
 
-	@Delegate(types = {Job.class}, excludes = {ExcludedJobMethods.class})
-	private Job job;
+    @Delegate(types = {Job.class}, excludes = {ExcludedJobMethods.class})
+    private Job job;
 
-	private String rootDirectory;
+    private String rootDirectory;
 
-	private InputValidator inputValidator;
+    private InputValidator inputValidator;
 
-	/**
-	 * create a jobService on existing job
-	 *
-	 * @param job
-	 */
-	public JobService(String rootDirectory, Job job) {
-		this.job = job;
-		this.rootDirectory = rootDirectory;
-		// TODO Exception si le job n'est pas persistent
-	}
+    /**
+     * create a jobService on existing job
+     *
+     * @param job
+     */
+    public JobService(String rootDirectory, Job job) {
+        this.job = job;
+        this.rootDirectory = rootDirectory;
+        // TODO Exception si le job n'est pas persistent
+    }
 
-	/**
-	 * create a new jobService
-	 *
-	 * @param referential : referential
-	 * @param action      : action
-	 * @param type        : type (may be null)
-	 * @throws mobi.chouette.service.ServiceException
-	 */
-	public JobService(String rootDirectory, String referential, String action, String type) throws ServiceException {
-		job = new Job(referential, action, type);
-		this.rootDirectory = rootDirectory;
+    /**
+     * create a new jobService
+     *
+     * @param referential : referential
+     * @param action      : action
+     * @param type        : type (may be null)
+     * @throws mobi.chouette.service.ServiceException
+     */
+    public JobService(String rootDirectory, String referential, String action, String type) throws ServiceException {
+        job = new Job(referential, action, type);
+        this.rootDirectory = rootDirectory;
 
-		if (!commandExists()) {
-			throw new RequestServiceException(RequestExceptionCode.UNKNOWN_ACTION, "Command does not exist: " + getCommandName());
-		}
-	}
+        if (!commandExists()) {
+            throw new RequestServiceException(RequestExceptionCode.UNKNOWN_ACTION, "Command does not exist: " + getCommandName());
+        }
+    }
 
-	/**
-	 * Read and save inputStreams as File
-	 *
-	 * @param inputStreamsByName
-	 * @throws ServiceException : if inputStream not valid with job
-	 */
-	public void saveInputStreams(final Map<String, InputStream> inputStreamsByName) throws ServiceException {
-		try {
-			if (!inputStreamsByName.containsKey(PARAMETERS_FILE)) {
-				throw new RequestServiceException(RequestExceptionCode.MISSING_PARAMETERS, "");
-			}
+    /**
+     * Read and save inputStreams as File
+     *
+     * @param inputStreamsByName
+     * @throws ServiceException : if inputStream not valid with job
+     */
+    public void saveInputStreams(final Map<String, InputStream> inputStreamsByName) throws ServiceException {
+        try {
+            if (!inputStreamsByName.containsKey(PARAMETERS_FILE)) {
+                throw new RequestServiceException(RequestExceptionCode.MISSING_PARAMETERS, "");
+            }
 
-			StringWriter writer = new StringWriter();
-			IOUtils.copy(inputStreamsByName.get(PARAMETERS_FILE), writer, "UTF-8");
+            StringWriter writer = new StringWriter();
+            IOUtils.copy(inputStreamsByName.get(PARAMETERS_FILE), writer, "UTF-8");
 
-			final InputValidator validator = getCommandInputValidator(getAction(), getType());
-			setParametersAsString(writer.toString());
-			writer.close();
-			Parameters parameters = new Parameters(getParametersAsString(), validator);
+            final InputValidator validator = getCommandInputValidator(getAction(), getType());
+            setParametersAsString(writer.toString());
+            writer.close();
+            Parameters parameters = new Parameters(getParametersAsString(), validator);
 
-			FileStore fileStore = FileStoreFactory.getFileStore();
-			log.info("using File Store for parameters file : " + fileStore.toString());
-			fileStore.writeFile(filePath(PARAMETERS_FILE), IOUtils.toInputStream(getParametersAsString() + "+n", "UTF-8"));
+            FileStore fileStore = FileStoreFactory.getFileStore();
+            log.info("using File Store for parameters file : " + fileStore.toString());
+            fileStore.writeFile(filePath(PARAMETERS_FILE), IOUtils.toInputStream(getParametersAsString() + "+n", "UTF-8"));
 
-			addLink(MediaType.APPLICATION_JSON, Link.PARAMETERS_REL);
+            addLink(MediaType.APPLICATION_JSON, Link.PARAMETERS_REL);
 
 
-			String inputStreamName = selectDataInputStreamName(inputStreamsByName);
-			if (inputStreamName != null) {
-				fileStore.writeFile(filePath(inputStreamName),inputStreamsByName.get(inputStreamName));
-				addLink(MediaType.APPLICATION_OCTET_STREAM, Link.DATA_REL);
-				addLink(MediaType.APPLICATION_OCTET_STREAM, Link.INPUT_REL);
-				job.setInputFilename(inputStreamName);
-			}
+            String inputStreamName = selectDataInputStreamName(inputStreamsByName);
+            if (inputStreamName != null) {
+                fileStore.writeFile(filePath(inputStreamName), inputStreamsByName.get(inputStreamName));
+                addLink(MediaType.APPLICATION_OCTET_STREAM, Link.DATA_REL);
+                addLink(MediaType.APPLICATION_OCTET_STREAM, Link.INPUT_REL);
+                job.setInputFilename(inputStreamName);
+            }
 
-			// Class.forName(getCommandInputValidatorName());
-			if (!validator.checkParameters(parameters.getConfiguration(), parameters.getValidation()))
-				throw new RequestServiceException(RequestExceptionCode.INVALID_PARAMETERS, "");
-			if (!validator.checkFilename(job.getInputFilename()))
-				throw new RequestServiceException(RequestExceptionCode.INVALID_FILE_FORMAT, "");
+            // Class.forName(getCommandInputValidatorName());
+            if (!validator.checkParameters(parameters.getConfiguration(), parameters.getValidation()))
+                throw new RequestServiceException(RequestExceptionCode.INVALID_PARAMETERS, "");
+            if (!validator.checkFilename(job.getInputFilename()))
+                throw new RequestServiceException(RequestExceptionCode.INVALID_FILE_FORMAT, "");
 
-			if (inputStreamName != null) {
-				if (!validator.checkFile(job.getInputFilename(), filePath(inputStreamName), parameters.getConfiguration()))
-					throw new RequestServiceException(RequestExceptionCode.INVALID_FORMAT, "");
-			}
-			
-			JSONUtil.toJSON(filePath(ACTION_PARAMETERS_FILE), parameters.getConfiguration());
-			fileStore.writeFile(filePath(ACTION_PARAMETERS_FILE), new ByteArrayInputStream(JSONUtil.toJSON(parameters.getValidation()).getBytes()));
-			addLink(MediaType.APPLICATION_JSON, Link.ACTION_PARAMETERS_REL);
+            if (inputStreamName != null) {
+                if (!validator.checkFile(job.getInputFilename(), filePath(inputStreamName), parameters.getConfiguration()))
+                    throw new RequestServiceException(RequestExceptionCode.INVALID_FORMAT, "");
+            }
 
-			if (parameters.getValidation() != null) {
-				fileStore.writeFile(filePath(VALIDATION_PARAMETERS_FILE), new ByteArrayInputStream(JSONUtil.toJSON(parameters.getValidation()).getBytes()));
-				addLink(MediaType.APPLICATION_JSON, Link.VALIDATION_PARAMETERS_REL);
-			}
+            JSONUtil.toJSON(filePath(ACTION_PARAMETERS_FILE), parameters.getConfiguration());
+            if (parameters.getConfiguration() != null) {
+                fileStore.writeFile(filePath(ACTION_PARAMETERS_FILE), new ByteArrayInputStream(JSONUtil.toJSON(parameters.getConfiguration()).getBytes()));
+                addLink(MediaType.APPLICATION_JSON, Link.ACTION_PARAMETERS_REL);
+            }
 
-			validator.initReport(this);
-			setStatus(Job.STATUS.SCHEDULED); // job is ready
+            if (parameters.getValidation() != null) {
+                fileStore.writeFile(filePath(VALIDATION_PARAMETERS_FILE), new ByteArrayInputStream(JSONUtil.toJSON(parameters.getValidation()).getBytes()));
+                addLink(MediaType.APPLICATION_JSON, Link.VALIDATION_PARAMETERS_REL);
+            }
 
-		} catch (ServiceException ex) {
-			throw ex;
-		} catch (Exception ex) {
-			Logger.getLogger(JobService.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
-			throw new RequestServiceException(RequestExceptionCode.INVALID_PARAMETERS, ex);
-		}
+            validator.initReport(this);
+            setStatus(Job.STATUS.SCHEDULED); // job is ready
 
-	}
+        } catch (ServiceException ex) {
+            throw ex;
+        } catch (Exception ex) {
+            Logger.getLogger(JobService.class.getName()).log(Level.SEVERE, ex.getMessage(), ex);
+            throw new RequestServiceException(RequestExceptionCode.INVALID_PARAMETERS, ex);
+        }
 
-	private String selectDataInputStreamName(final Map<String, InputStream> inputStreamsByName) {
-		for (String name : inputStreamsByName.keySet()) {
-			if (!name.equals(PARAMETERS_FILE)) {
-				return name;
-			}
-		}
-		return null;
-	}
+    }
 
-	private boolean jobPersisted() {
-		return job.getId() != null;
-	}
+    private String selectDataInputStreamName(final Map<String, InputStream> inputStreamsByName) {
+        for (String name : inputStreamsByName.keySet()) {
+            if (!name.equals(PARAMETERS_FILE)) {
+                return name;
+            }
+        }
+        return null;
+    }
 
-	private java.nio.file.Path filePath(String fileName) {
-		return Paths.get(getPathName(), fileName);
-	}
+    private boolean jobPersisted() {
+        return job.getId() != null;
+    }
 
-	private Parameters getParameters() throws ServiceException {
-		if (jobPersisted()) {
-			try {
-				return new Parameters(getParametersAsString(), getCommandInputValidator(getAction(), getType()));
-				// return JSONUtil.fromJSON( getParametersAsString(),
-				// Parameters.class);
-			} catch (Exception ex) {
-				try {
-					return new Parameters(getParametersAsString(), inputValidator);
-				} catch (Exception e) {
-					return null;
-				}
-				// throw new
-				// RequestServiceException(RequestExceptionCode.INVALID_PARAMETERS,
-				// ex);
-			}
-		}
-		return null;
-	}
+    private java.nio.file.Path filePath(String fileName) {
+        return Paths.get(getPathName(), fileName);
+    }
 
-	public AbstractParameter getActionParameter() throws ServiceException {
-		if (jobPersisted()) {
-			return getParameters().getConfiguration();
-		}
-		return null;
-	}
+    private Parameters getParameters() throws ServiceException {
+        if (jobPersisted()) {
+            try {
+                return new Parameters(getParametersAsString(), getCommandInputValidator(getAction(), getType()));
+                // return JSONUtil.fromJSON( getParametersAsString(),
+                // Parameters.class);
+            } catch (Exception ex) {
+                try {
+                    return new Parameters(getParametersAsString(), inputValidator);
+                } catch (Exception e) {
+                    return null;
+                }
+                // throw new
+                // RequestServiceException(RequestExceptionCode.INVALID_PARAMETERS,
+                // ex);
+            }
+        }
+        return null;
+    }
 
-	public ValidationParameters getValidationParameter() throws ServiceException {
-		if (jobPersisted()) {
-			return getParameters().getValidation();
-		}
-		return null;
-	}
+    public AbstractParameter getActionParameter() throws ServiceException {
+        if (jobPersisted()) {
+            return getParameters().getConfiguration();
+        }
+        return null;
+    }
 
-	/**
-	 * return job file path <br/>
-	 * build it if not set and job saved
-	 *
-	 * @return path or null if job not saved
-	 */
-	public String getPathName() {
-		if (jobPersisted()) {
-			return Paths.get(rootDirectory, ROOT_PATH, job.getReferential(), "data", job.getId().toString()).toString();
-		}
-		// TODO Non, lever une exception
-		return null;
-	}
+    public ValidationParameters getValidationParameter() throws ServiceException {
+        if (jobPersisted()) {
+            return getParameters().getValidation();
+        }
+        return null;
+    }
 
-	public static String getRootPathName(String rootDirectory, String referential) {
-		return Paths.get(rootDirectory, ROOT_PATH, referential).toString();
-	}
+    /**
+     * return job file path <br/>
+     * build it if not set and job saved
+     *
+     * @return path or null if job not saved
+     */
+    public String getPathName() {
+        if (jobPersisted()) {
+            return Paths.get(rootDirectory, ROOT_PATH, job.getReferential(), "data", job.getId().toString()).toString();
+        }
+        // TODO Non, lever une exception
+        return null;
+    }
 
-	public java.nio.file.Path getPath() {
-		if (jobPersisted()) {
-			return java.nio.file.Paths.get(getPathName());
-		}
-		// TODO Non, lever une exception
-		return null;
-	}
+    public static String getRootPathName(String rootDirectory, String referential) {
+        return Paths.get(rootDirectory, ROOT_PATH, referential).toString();
+    }
 
-	/**
-	 * add a link or replace
-	 *
-	 * @param mediaType : mime type
-	 * @param rel       : link key
-	 */
-	public void addLink(String mediaType, String rel) {
-		linkRemove(rel);
-		Link link = new Link(mediaType, rel);
-		job.getLinks().add(link);
+    public java.nio.file.Path getPath() {
+        if (jobPersisted()) {
+            return java.nio.file.Paths.get(getPathName());
+        }
+        // TODO Non, lever une exception
+        return null;
+    }
 
-	}
+    /**
+     * add a link or replace
+     *
+     * @param mediaType : mime type
+     * @param rel       : link key
+     */
+    public void addLink(String mediaType, String rel) {
+        linkRemove(rel);
+        Link link = new Link(mediaType, rel);
+        job.getLinks().add(link);
 
-	/**
-	 * check link existence
-	 *
-	 * @param rel link key
-	 * @return
-	 */
-	public boolean linkExists(String rel) {
-		for (Link link : job.getLinks()) {
-			if (link.getRel().equals(rel)) {
-				return true;
-			}
-		}
-		return false;
-	}
+    }
 
-	/**
-	 * check link existence
-	 *
-	 * @param rel link key
-	 * @return
-	 */
-	public Link linkRemove(String rel) {
-		for (Iterator<Link> iterator = job.getLinks().iterator(); iterator.hasNext(); ) {
-			Link link = iterator.next();
-			if (link.getRel().equals(rel)) {
-				iterator.remove();
-				return link;
-			}
-		}
-		return null;
-	}
+    /**
+     * check link existence
+     *
+     * @param rel link key
+     * @return
+     */
+    public boolean linkExists(String rel) {
+        for (Link link : job.getLinks()) {
+            if (link.getRel().equals(rel)) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-	/**
-	 * remove a link if exists <br/>
-	 * does nothing if not
-	 *
-	 * @param rel link key
-	 */
-	public void removeLink(String rel) {
-		for (Iterator<Link> iterator = job.getLinks().iterator(); iterator.hasNext(); ) {
-			Link link = iterator.next();
-			if (link.getRel().equals(rel)) {
-				iterator.remove();
-				break;
-			}
-		}
-	}
+    /**
+     * check link existence
+     *
+     * @param rel link key
+     * @return
+     */
+    public Link linkRemove(String rel) {
+        for (Iterator<Link> iterator = job.getLinks().iterator(); iterator.hasNext(); ) {
+            Link link = iterator.next();
+            if (link.getRel().equals(rel)) {
+                iterator.remove();
+                return link;
+            }
+        }
+        return null;
+    }
 
-	private boolean commandExists() {
-		try {
-			Class.forName(getCommandName());
-		} catch (ClassNotFoundException e) {
-			return false;
-		}
-		return true;
-	}
+    /**
+     * remove a link if exists <br/>
+     * does nothing if not
+     *
+     * @param rel link key
+     */
+    public void removeLink(String rel) {
+        for (Iterator<Link> iterator = job.getLinks().iterator(); iterator.hasNext(); ) {
+            Link link = iterator.next();
+            if (link.getRel().equals(rel)) {
+                iterator.remove();
+                break;
+            }
+        }
+    }
 
-	public String getCommandName() {
-		String type = getType() == null ? "" : getType();
-		return "mobi.chouette.exchange." + (type.isEmpty() ? "" : type + ".") + getAction() + "."
-				+ StringUtils.capitalize(type) + StringUtils.capitalize(getAction()) + "Command";
-	}
+    private boolean commandExists() {
+        try {
+            Class.forName(getCommandName());
+        } catch (ClassNotFoundException e) {
+            return false;
+        }
+        return true;
+    }
+
+    public String getCommandName() {
+        String type = getType() == null ? "" : getType();
+        return "mobi.chouette.exchange." + (type.isEmpty() ? "" : type + ".") + getAction() + "."
+                + StringUtils.capitalize(type) + StringUtils.capitalize(getAction()) + "Command";
+    }
 
 //	private InputValidator getCommandInputValidator() throws Exception {
 //		if (inputValidator == null) {
@@ -302,29 +304,29 @@ public class JobService implements JobData, ServiceConstants {
 //	}
 
 
-	public static InputValidator getCommandInputValidator(String actionParam, String typeParam) throws ClassNotFoundException, IOException {
-		String type = typeParam == null ? "" : typeParam;
-		final InputValidator inputValidator = InputValidatorFactory.create("mobi.chouette.exchange."
-				+ (type.isEmpty() ? "" : type + ".") + actionParam + "." + StringUtils.capitalize(type)
-				+ StringUtils.capitalize(actionParam) + "InputValidator");
-		return inputValidator;
-	}
+    public static InputValidator getCommandInputValidator(String actionParam, String typeParam) throws ClassNotFoundException, IOException {
+        String type = typeParam == null ? "" : typeParam;
+        final InputValidator inputValidator = InputValidatorFactory.create("mobi.chouette.exchange."
+                + (type.isEmpty() ? "" : type + ".") + actionParam + "." + StringUtils.capitalize(type)
+                + StringUtils.capitalize(actionParam) + "InputValidator");
+        return inputValidator;
+    }
 
 
-	public Set<String> getRequiredReferentialsLocks() {
-		Set<String> requiredLocks = new HashSet<>();
-		requiredLocks.add(getReferential());
-		log.debug("Adding required locks for job: "+job);
+    public Set<String> getRequiredReferentialsLocks() {
+        Set<String> requiredLocks = new HashSet<>();
+        requiredLocks.add(getReferential());
+        log.debug("Adding required locks for job: " + job);
 
-		try {
-			if (EXPORTER.equals(getAction()) && "transfer".equals(getType()) && getActionParameter() instanceof AbstractExportParameter) {
-				log.debug("Adding required locks for export job: "+job);
-				requiredLocks.addAll(((AbstractExportParameter) getActionParameter()).getAdditionalRequiredReferentialLocks());
-			}
-		} catch (ServiceException serviceException) {
-			throw new RuntimeException("Failed to access action parameters for transfer job: " + serviceException.getMessage(), serviceException);
-		}
+        try {
+            if (EXPORTER.equals(getAction()) && "transfer".equals(getType()) && getActionParameter() instanceof AbstractExportParameter) {
+                log.debug("Adding required locks for export job: " + job);
+                requiredLocks.addAll(((AbstractExportParameter) getActionParameter()).getAdditionalRequiredReferentialLocks());
+            }
+        } catch (ServiceException serviceException) {
+            throw new RuntimeException("Failed to access action parameters for transfer job: " + serviceException.getMessage(), serviceException);
+        }
 
-		return requiredLocks;
-	}
+        return requiredLocks;
+    }
 }
