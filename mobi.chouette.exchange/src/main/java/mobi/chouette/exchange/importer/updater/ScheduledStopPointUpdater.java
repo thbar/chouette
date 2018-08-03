17c8f33f09f7ed8364fc5da03db56a1ab27b0b85
Mathieu Hicauber <mhicauber@okina.fr>
2018-08-03 15:00:36 +0200

    fixed NPE on new SP import

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/updater/ScheduledStopPointUpdater.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/updater/ScheduledStopPointUpdater.java
@@ -135,7 +135,7 @@ public class ScheduledStopPointUpdater implements Updater<ScheduledStopPoint> {
 			} else if (data.getDataLocations() == null) {
 				log.error("ValidationData.getDataLocations() is null");
 			} else if (newSA!=null && data.getDataLocations().get(newSA.getObjectId()) == null) {
-				log.error("No data location for new stop area: " + newSA.getObjectId() + " unlike old stop area: " + oldSA.getObjectId());
+				log.error("No data location for new stop area: " + newSA.getObjectId() + " unlike old stop area: " + oldSA == null ? "null" : oldSA.getObjectId());
 			}
 
 
