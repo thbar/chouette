22aac5f75f31e168d9ebcb1fc1101388fdf1818a
Grégoire Fora <gfora@okina.fr>
2019-06-21 08:17:19 +0200

    Add producers names for ratp test

--- mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/NetworksNames.java
+++ mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/NetworksNames.java
@@ -61,6 +61,9 @@ public class NetworksNames {
         producers.put("BAC","Transports Maritimes Départementaux de la Gironde");
         producers.put("FAI","Liaison maritime Aix-Fouras");
 //        producers.put("SNC","SNCF");
+        producers.put("T1", "Test 1");
+        producers.put("T2", "Test 2");
+        producers.put("T3", "Test 3");
 
 
         producersExceptionNameNetwork.add("COU");
