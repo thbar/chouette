eb1f1028f2b860bb51a64e264d6e9ab5ed8c269c
Grégoire Fora <gfora@okina.fr>
2019-11-25 16:59:13 +0100

    Change NetexProfileVersion split

--- mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/model/NetexProfileVersion.java
+++ mobi.chouette.exchange.netexprofile/src/main/java/mobi/chouette/exchange/netexprofile/model/NetexProfileVersion.java
@@ -3,10 +3,11 @@ package mobi.chouette.exchange.netexprofile.model;
 public class NetexProfileVersion {
 	
 	//1.04:NO-NeTEx-networktimetable:1.0
+	// Split changer pour gérer le profil IDFM
 	public static String getSchemaVersion(String fullProfileString) {
 		String[] split = fullProfileString.split(":");
 	
-		if(split.length == 3) {
+		if(split.length == 2) {
 			// Valid
 			return split[0];
 		}
