2d2aab6352dddc8406f8f9618c8f15ab13c207fe
Tony <tony.lisambert@icloud.com>
2019-12-12 11:19:53 +0100

    test: parse xml unmarshal

--- mobi.chouette.exchange.stopplace/src/test/java/mobi/chouette/exchange/stopplace/PublicationDeliveryStopPlaceParserTest.java
+++ mobi.chouette.exchange.stopplace/src/test/java/mobi/chouette/exchange/stopplace/PublicationDeliveryStopPlaceParserTest.java
@@ -2,15 +2,26 @@ package mobi.chouette.exchange.stopplace;
 
 import com.google.common.collect.Sets;
 
+import mobi.chouette.exchange.netexprofile.jaxb.NetexXMLProcessingHelperFactory;
 import mobi.chouette.model.StopArea;
 
+import net.sf.saxon.s9api.XdmNode;
+import org.rutebanken.netex.model.PublicationDeliveryStructure;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
+import java.io.File;
 import java.io.FileInputStream;
 import java.util.Collection;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+
+import static javax.xml.bind.JAXBContext.newInstance;
 
 public class PublicationDeliveryStopPlaceParserTest {
 
@@ -37,4 +48,20 @@ public class PublicationDeliveryStopPlaceParserTest {
 
 
     }
+
+
+
+    @Test
+    public void testIDFM() throws Exception {
+
+        NetexXMLProcessingHelperFactory importer = new NetexXMLProcessingHelperFactory();
+        File file = new File("src/test/resources/netex/IDFM_Reflex_all.xml");
+
+        XdmNode dom = importer.parseFileToXdmNode(file, new HashSet<>());
+        PublicationDeliveryStructure unmarshal = importer.unmarshal(file, new HashSet<>());
+
+        System.out.println(unmarshal.getDataObjects());
+
+    }
+
 }
