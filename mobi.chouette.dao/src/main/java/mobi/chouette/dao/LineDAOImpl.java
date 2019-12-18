4aa658422f2412bb50982cf4678249088613d4d5
schabanne <schabanne@okina.fr>
2019-12-18 11:53:36 +0100

    GTFS - fix remontée erreur sql

--- mobi.chouette.dao/src/main/java/mobi/chouette/dao/LineDAOImpl.java
+++ mobi.chouette.dao/src/main/java/mobi/chouette/dao/LineDAOImpl.java
@@ -21,13 +21,18 @@ public class LineDAOImpl extends GenericDAOImpl<Line> implements LineDAO {
 
 	public String updateStopareasForIdfmLineCommand(Long lineId) throws Exception
 	{
-		String retour;
+		String retour = null;
 		try {
 			retour = (String) em.createNativeQuery("SELECT update_sa_for_idfm_line FROM update_sa_for_idfm_line(:lineId);")
 					.setParameter("lineId", lineId)
 					.getSingleResult();
 		} catch (PersistenceException e){
-			throw new Exception(e.getCause());
+			if(e.getCause().getCause().getMessage().contains("MOSAIC_SQL_ERROR:")){
+				String[] splitError = e.getCause().getCause().getMessage().split("MOSAIC_SQL_ERROR:");
+				throw new Exception("MOSAIC_SQL_ERROR:" + splitError[1]);
+			} else {
+				// swallow : on ne fait pas d l'idfm
+			}
 		}
 
 		return retour;
