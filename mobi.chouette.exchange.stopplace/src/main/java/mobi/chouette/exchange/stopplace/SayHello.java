e065e26654568f740f452a8ccc49c01c45344558
Tony <tony.lisambert@icloud.com>
2019-12-24 10:00:21 +0100

    feat: PublicationDelivery Update ZDEP ZDER ZDLR by Ref iev properties URL delete bad commit files

--- mobi.chouette.exchange.stopplace/src/main/java/mobi/chouette/exchange/stopplace/SayHello.java
+++
@@ -1,7 +0,0 @@
-package mobi.chouette.exchange.stopplace;
-
-public interface SayHello {
-
-    public String sayHello(String name) throws Exception;
-    public String sayGoodbye() throws Exception;
-}
