3f024ae4a5f7a5b44479f32d71f21501de96101a
schabanne <schabanne@okina.fr>
2020-11-12 14:30:06 +0100

    delete lines without offer

--- mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/DeleteLineWithoutOfferCommand.java
+++ mobi.chouette.exchange/src/main/java/mobi/chouette/exchange/importer/DeleteLineWithoutOfferCommand.java
@@ -6,13 +6,16 @@ import mobi.chouette.common.Context;
 import mobi.chouette.common.chain.Command;
 import mobi.chouette.common.chain.CommandFactory;
 import mobi.chouette.dao.LineDAO;
+import mobi.chouette.dao.NetworkDAO;
 import mobi.chouette.model.Line;
+import mobi.chouette.model.Network;
 
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 import java.io.IOException;
+import java.util.List;
 
 @Stateless(name = DeleteLineWithoutOfferCommand.COMMAND)
 @Log4j
@@ -21,15 +24,30 @@ public class DeleteLineWithoutOfferCommand implements Command, Constant {
     @EJB
     LineDAO lineDAO;
 
+    @EJB
+    NetworkDAO networkDAO;
+
     public static final String COMMAND = "DeleteLineWithoutOfferCommand";
 
     @Override
     public boolean execute(Context context) throws Exception {
+        // suppression des lignes sans offre
         for(Line line : lineDAO.findAll()){
             if(line.getRoutes().size()==0)
                 lineDAO.delete(line);
         }
         lineDAO.flush(); // to prevent SQL error outside method
+
+        // du coup on supprime les réseaux sans ligne car inutiles
+        for(Network network: networkDAO.findAll()) {
+            List<Line> lines = lineDAO.findByNetworkId(network.getId());
+            boolean hasLines = lines != null && lines.size() > 0;
+            if(!hasLines){
+                networkDAO.delete(network);
+            }
+        }
+        networkDAO.flush(); // to prevent SQL error outside method
+
         return SUCCESS;
     }
 
