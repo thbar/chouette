9ea02c3e33672b310344808523950cf83f085f06
Mathieu Hicauber <mhicauber@okina.fr>
2021-01-15 16:09:49 +0100

    Build docker

---
+++ docker/create-dev-container.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+# Créer le container depuis l'image de base , avec les props nécessaire pour la datasource et la
+# surcharge de la commande avec le --debug
+docker run \
+-p 8080:8080 \
+-p 8787:8787 \
+-p 9990:9990 \
+--name na-chouette-dev \
+--env CHOUETTE_DB_HOST=192.168.24.59 \
+--env CHOUETTE_DB_PORT=5449 \
+--env CHOUETTE_DB_USER=chouette \
+--env CHOUETTE_DB_PASSWORD=chouette \
+--env CHOUETTE_DB_NAME=chouette \
+--env JAVA_OPTS="-Xms1024m -Xmx2048m -XX:MetaspaceSize=256M -XX:MaxMetaspaceSize=512m -Djava.net.preferIPv4Stack=true -Djboss.modules.system.pkgs=org.jboss.byteman -Djava.awt.headless=true" \
+registry.okina.fr/mobiiti/chouette-base:1.0 \
+/opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0 --debug
---
+++ docker/docker-entrypoint.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+INIT_FILE="/opt/jboss/initialized.done"
+
+function waitWildfly {
+	status=""
+	while [ "$status" != "running" ];do
+		status=$(/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --commands="read-attribute server-state")
+		if [ "$status" != "running" ]; then
+			echo "Waiting for Wildfly..."
+			sleep 1
+		fi
+	done
+}
+
+function initWildfly {
+	waitWildfly
+	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --file=/tmp/wildfly_db.cli
+	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --command='/subsystem=transactions:write-attribute(name=default-timeout,value=3600)'
+	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --command='/subsystem=undertow/server=default-server/http-listener=default/:write-attribute(name=max-post-size,value=25485760)'
+	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --command="/:reload"
+	/opt/jboss/wildfly/bin/jboss-cli.sh -c --user=admin --password=password --command="deploy /tmp/chouette.ear"
+	touch $INIT_FILE
+}
+
+function waitPostgres {
+  status="closed"
+  while [[ $status == *"closed"* ]];do
+    status=$(nmap -v -p 5432 chouette-postgres)
+    echo 'Waiting for Postgres...'
+    sleep 2
+  done
+  exec $@
+}
+
+[ ! -e $INIT_FILE ] && initWildfly &
+
+waitPostgres $@
+
