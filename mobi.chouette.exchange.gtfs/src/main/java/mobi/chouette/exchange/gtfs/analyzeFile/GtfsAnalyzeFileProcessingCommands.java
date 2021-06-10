a0c83fff3707675914d8f42e95a804af4f21012b
gcarrere <gcarrere@okina.fr>
2021-06-10 15:41:24 +0200

    10117 - new import console V1

---
+++ mobi.chouette.exchange.gtfs/src/main/java/mobi/chouette/exchange/gtfs/analyzeFile/GtfsAnalyzeFileProcessingCommands.java
@@ -0,0 +1,168 @@
+package mobi.chouette.exchange.gtfs.analyzeFile;
+
+import lombok.Data;
+import lombok.extern.log4j.Log4j;
+import mobi.chouette.common.Constant;
+import mobi.chouette.common.Context;
+import mobi.chouette.common.chain.Chain;
+import mobi.chouette.common.chain.ChainCommand;
+import mobi.chouette.common.chain.Command;
+import mobi.chouette.common.chain.CommandFactory;
+import mobi.chouette.exchange.ProcessingCommands;
+import mobi.chouette.exchange.ProcessingCommandsFactory;
+import mobi.chouette.exchange.fileAnalysis.ProcessAnalyzeCommand;
+import mobi.chouette.exchange.gtfs.importer.GtfsImportParameters;
+import mobi.chouette.exchange.gtfs.importer.GtfsInitImportCommand;
+import mobi.chouette.exchange.gtfs.importer.GtfsRouteParserCommand;
+import mobi.chouette.exchange.gtfs.importer.GtfsStopParserCommand;
+import mobi.chouette.exchange.gtfs.importer.GtfsValidationCommand;
+import mobi.chouette.exchange.gtfs.importer.GtfsValidationRulesCommand;
+import mobi.chouette.exchange.gtfs.model.GtfsRoute;
+import mobi.chouette.exchange.gtfs.model.importer.GtfsImporter;
+import mobi.chouette.exchange.gtfs.model.importer.Index;
+import mobi.chouette.exchange.importer.LineRegisterCommand;
+import mobi.chouette.exchange.importer.UncompressCommand;
+import org.apache.commons.lang.StringUtils;
+
+import javax.naming.InitialContext;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Log4j
+public class GtfsAnalyzeFileProcessingCommands implements ProcessingCommands, Constant {
+
+    public static class DefaultFactory extends ProcessingCommandsFactory {
+
+        @Override
+        protected ProcessingCommands create() throws IOException {
+            ProcessingCommands result = new GtfsAnalyzeFileProcessingCommands();
+            return result;
+        }
+    }
+
+    static {
+        ProcessingCommandsFactory.factories.put(GtfsAnalyzeFileProcessingCommands.class.getName(), new DefaultFactory());
+    }
+
+    @Override
+    public List<? extends Command> getPreProcessingCommands(Context context, boolean withDao) {
+        InitialContext initialContext = (InitialContext) context.get(INITIAL_CONTEXT);
+        GtfsImportParameters parameters = (GtfsImportParameters) context.get(CONFIGURATION);
+        List<Command> commands = new ArrayList<>();
+        try {
+            commands.add(CommandFactory.create(initialContext, UncompressCommand.class.getName()));
+            commands.add(CommandFactory.create(initialContext, GtfsValidationRulesCommand.class.getName()));
+            commands.add(CommandFactory.create(initialContext, GtfsInitImportCommand.class.getName()));
+            commands.add(CommandFactory.create(initialContext, GtfsValidationCommand.class.getName()));
+        } catch (Exception e) {
+            log.error(e, e);
+            throw new RuntimeException("unable to call factories");
+        }
+        return commands;
+    }
+
+    @Override
+    public List<? extends Command> getLineProcessingCommands(Context context, boolean withDao) {
+        InitialContext initialContext = (InitialContext) context.get(INITIAL_CONTEXT);
+        GtfsImportParameters parameters = (GtfsImportParameters) context.get(CONFIGURATION);
+        boolean level3validation = context.get(VALIDATION) != null;
+        List<Command> commands = new ArrayList<>();
+        GtfsImporter importer = (GtfsImporter) context.get(PARSER);
+        Index<GtfsRoute> index = importer.getRouteById();
+
+        try {
+            {
+                Chain chain = (Chain) CommandFactory.create(initialContext, ChainCommand.class.getName());
+                chain.add(CommandFactory.create(initialContext, GtfsStopParserCommand.class.getName()));
+//				if (withDao && !parameters.isNoSave() && parameters.getStopAreaImportMode().shouldCreateMissingStopAreas()) {
+//					Command saveArea = CommandFactory.create(initialContext, StopAreaRegisterCommand.class.getName());
+//					chain.add(saveArea);
+//				}
+                commands.add(chain);
+            }
+
+            {
+//                Chain chain = (Chain) CommandFactory.create(initialContext, ChainCommand.class.getName());
+//                Command productionPeriods = CommandFactory.create(initialContext, ProductionPeriodCommand.class.getName());
+//                chain.add(productionPeriods);
+//                commands.add(chain);
+            }
+
+            ArrayList<String> savedLines = new ArrayList<String>();
+            Integer cpt = 1;
+
+            String splitCharacter = parameters.getSplitCharacter();
+            for (GtfsRoute gtfsRoute : index) {
+
+                if (StringUtils.isNotEmpty(splitCharacter)){
+                    String newRouteId = gtfsRoute.getRouteId().split(parameters.getSplitCharacter())[0];
+                    if(parameters.getRouteMerge() && savedLines.contains(newRouteId)) continue;
+                    savedLines.add(newRouteId);
+                    gtfsRoute.setRouteId(newRouteId.replaceFirst("^"+parameters.getLinePrefixToRemove(),""));
+                }
+
+
+
+                Chain chain = (Chain) CommandFactory.create(initialContext, ChainCommand.class.getName());
+
+                GtfsRouteParserCommand parser = (GtfsRouteParserCommand) CommandFactory.create(initialContext,
+                        GtfsRouteParserCommand.class.getName());
+                parser.setGtfsRouteId(gtfsRoute.getRouteId().replaceFirst("^"+parameters.getLinePrefixToRemove(),""));
+                parser.setPosition(cpt);
+                cpt++;
+                chain.add(parser);
+
+
+                // register
+                Command analyzeCommand = CommandFactory.create(initialContext, ProcessAnalyzeCommand.class.getName());
+                chain.add(analyzeCommand);
+
+                commands.add(chain);
+            }
+
+
+
+        } catch (Exception e) {
+            log.error(e, e);
+            throw new RuntimeException("unable to call factories");
+        }
+
+        return commands;
+    }
+
+    @Override
+    public List<? extends Command> getStopAreaProcessingCommands(Context context, boolean withDao) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<? extends Command> getPostProcessingCommands(Context context, boolean withDao) {
+        List<Command> commands = new ArrayList<>();
+//        InitialContext initialContext = (InitialContext) context.get(INITIAL_CONTEXT);
+//        try {
+//            commands.add(CommandFactory.create(initialContext, ProcessAnalyzeCommand.class.getName()));
+//        } catch (ClassNotFoundException | IOException e) {
+//            log.error(e, e);
+//            throw new RuntimeException("unable to call factories");
+//        }
+        return commands;
+    }
+
+    @Override
+    public List<? extends Command> getPostProcessingCommands(Context context, boolean withDao, boolean allSchemas) {
+        return new ArrayList<>();
+    }
+
+    @Override
+    public List<? extends Command> getDisposeCommands(Context context, boolean withDao) {
+        return  new ArrayList<>();
+    }
+
+    @Override
+    public List<? extends Command> getMobiitiCommands(Context context, boolean b) {
+        return new ArrayList<>();
+    }
+
+}
---
+++ mobi.chouette.exchange.neptune/src/main/java/mobi/chouette/exchange/neptune/analyzeFile/NeptuneAnalyzeFileCommand.java
@@ -0,0 +1,76 @@
+package mobi.chouette.exchange.neptune.analyzeFile;
+
+import com.jamonapi.Monitor;
+import com.jamonapi.MonitorFactory;
+import lombok.extern.log4j.Log4j;
+import mobi.chouette.common.Context;
+import mobi.chouette.common.chain.Command;
+import mobi.chouette.common.chain.CommandFactory;
+import mobi.chouette.exchange.ProcessingCommands;
+import mobi.chouette.exchange.ProcessingCommandsFactory;
+import mobi.chouette.exchange.ProgressionCommand;
+import mobi.chouette.exchange.importer.AbstractImporterCommand;
+import mobi.chouette.exchange.neptune.Constant;
+import mobi.chouette.exchange.neptune.importer.NeptuneImportParameters;
+import mobi.chouette.exchange.report.ActionReport;
+import mobi.chouette.exchange.report.ActionReporter;
+import mobi.chouette.exchange.report.AnalyzeReport;
+
+import javax.naming.InitialContext;
+import java.io.IOException;
+
+@Log4j
+public class NeptuneAnalyzeFileCommand extends AbstractImporterCommand implements Command, Constant {
+
+    public static final String COMMAND = "NeptuneAnalyzeFileCommand";
+
+
+    @Override
+    public boolean execute(Context context) throws Exception {
+
+        boolean result = ERROR;
+        Monitor monitor = MonitorFactory.start(COMMAND);
+        InitialContext initialContext = (InitialContext) context.get(INITIAL_CONTEXT);
+        ActionReport report = (ActionReport) context.get(REPORT);
+        ProgressionCommand progression = (ProgressionCommand) CommandFactory.create(initialContext, ProgressionCommand.class.getName());
+        ActionReporter reporter = ActionReporter.Factory.getInstance();
+
+        AnalyzeReport analyzeReport = new AnalyzeReport();
+        context.put(ANALYSIS_REPORT, analyzeReport);
+
+        // check params
+        Object configuration = context.get(CONFIGURATION);
+        if (!(configuration instanceof NeptuneImportParameters)) {
+            reporter.setActionError(context, ActionReporter.ERROR_CODE.INVALID_PARAMETERS,"invalid parameters for gtfs import " + configuration.getClass().getName());
+            return ERROR;
+        }
+
+        NeptuneImportParameters parameters = (NeptuneImportParameters) configuration;
+        ProcessingCommands commands = ProcessingCommandsFactory.create(NeptuneAnalyzeFileProcessingCommands.class.getName());
+
+        try{
+            result = process(context, commands, progression, true, Mode.line);
+            log.info("Neptune analysis completed");
+            report.setResult("OK");
+            progression.saveAnalyzeReport(context,true);
+        }catch(Exception e){
+            report.setResult("NOK");
+            return false;
+        }
+
+        return true;
+    }
+
+    public static class DefaultCommandFactory extends CommandFactory {
+
+        @Override
+        protected Command create(InitialContext context) throws IOException {
+            Command result = new NeptuneAnalyzeFileCommand();
+            return result;
+        }
+    }
+
+    static {
+        CommandFactory.factories.put(NeptuneAnalyzeFileCommand.class.getName(), new NeptuneAnalyzeFileCommand.DefaultCommandFactory());
+    }
+}
